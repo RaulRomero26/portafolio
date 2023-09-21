b9107c6072394be5ed5401844ddb68e65edd0f02
Raul Romero <link_2642@hotmail.com>
2023-09-21 16:39:57 -0600

    update: starting the styles

---
+++ src/components/Showcase.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+import {
+    PaddingContainer,
+    FlexContainer,
+    Heading,
+    ParaText,
+    BlueText,
+    IconContainer,
+} from '../styles/Global.styled'
+
+export const Showcase = () => {
+  return (
+    <PaddingContainer
+        id="Home"
+        left="3%"
+        right="10%"
+        top="15%"
+        bottom="10%"
+    >
+        <FlexContainer align="center" fullWidthChild>
+
+        </FlexContainer>
+    </PaddingContainer>
+  )
+}
