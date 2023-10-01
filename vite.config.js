e3494433117f9f54ddbdb4bffaa30c8bf577556c
RaúlRomero <55547165+RaulRomero26@users.noreply.github.com>
2023-09-30 22:23:17 -0600

    Update: new domain!
    http://www.raul-romero.com/ as new domain!

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  base: 'http://www.raul-romero.com/',
   plugins: [react()],
 })
