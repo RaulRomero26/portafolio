db4b39f734042861dca9a7bcad6bbf9f54879750
Raul Romero <link_2642@hotmail.com>
2023-10-01 20:03:46 -0600

    update: added fade image

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // import React from 'react'
 
-import { Container, MainBody } from "./styles/Global.styled";
+import { Container, MainBody,FadeImage } from "./styles/Global.styled";
 import { ThemeProvider } from "styled-components";
 import { theme } from './utils/Theme';
 
@@ -11,7 +11,10 @@ import { Footer } from './components/Footer';
 import { Navbar } from './components/Navbar';
 //import componets
 
-
+import TopFadeImage from './assets/top.png' 
+import RightFadeImage from './assets/right.png' 
+import LeftFadeImage from './assets/left.png' 
+import BottomFadeImage from './assets/bottom.png' 
 
 function App() {
 
@@ -26,6 +29,12 @@ function App() {
           <MyProjects/>
           <Footer/>
         </Container>
+
+        <FadeImage src={TopFadeImage} $top="0"/>
+        <FadeImage src={LeftFadeImage} $top="30vh"/>
+        <FadeImage src={RightFadeImage} $top="300%" $right="0" />
+        <FadeImage src={RightFadeImage} $top="120%" $right="0" />
+        <FadeImage src={LeftFadeImage} $top="200%"/>
       </MainBody> 
     </ThemeProvider>
     </>
