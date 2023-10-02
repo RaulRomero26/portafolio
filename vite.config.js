7b2eb1a7063690cd339258833be8e12c603006c6
Raul Romero <link_2642@hotmail.com>
2023-10-01 20:46:37 -0600

    fix: try 2

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://www.raul-romero.com/',
+  base: 'https://raul-romero.com/',
   plugins: [react()],
 })
